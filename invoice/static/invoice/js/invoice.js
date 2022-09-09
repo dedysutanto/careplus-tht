a8b7a23ecc447db1c014b1ed52db08a19d4a6eab
gkiserpong <gkiserpongit@gmail.com>
2022-09-09 22:32:52 +0700

    DateTime

--- invoice/static/invoice/js/invoice.js
+++ invoice/static/invoice/js/invoice.js
@@ -14,6 +14,11 @@ $(document).ready(function() {
         let elem = $('div').find('[data-contentpath="is_final"]');
         elem.append('<input id="id_is_final" type="hidden" name="is_final" value="true">');
 
+        $('#id_datetime').prop('disabled', true).prop('name', '');
+        let elem_datetime = $('div').find('[data-contentpath="datetime"]');
+        let datetime_value = elem_datetime.find('#id_datetime').val();
+        elem_datetime.append('<input id="id_datetime" type="hidden" name="datetime" value="' + datetime_value + '">');
+
         //console.log($('div').find('[data-contentpath="is_final"]'));
         if ($('#id_is_cancel').is(":checked")) {
             $('#id_is_cancel').prop('disabled', true).prop('name', '');
